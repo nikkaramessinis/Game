2e2fc74a5d9decf50743b8f2f9c01348fed4a2f6
nikkaramessinis <nikaramessinis@gmail.com>
2020-05-02 17:55:07 +0100

    Limiting Frame Rate

--- main.cpp
+++ main.cpp
@@ -5,13 +5,30 @@ std::unique_ptr<Game> game;
 
 int main(int argc, const char* argv[]) {
 
+  const int FPS = 60;
+  const int frameDelay = 1000/FPS;
+
+  Uint32 frameStart;
+  int frameTime;
+  
   game = std::make_unique<Game>();
   game->init("KaramEngine" , SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
   while (game->running())
   {
+    frameStart = SDL_GetTicks();
+    
     game->handleEvents();  
     game->update();  
-    game->render();  
+    game->render();
+
+    // How long the frame has taken
+    frameTime = SDL_GetTicks() - frameStart;
+    
+    if (frameDelay > frameTime)
+    {
+      SDL_Delay(frameDelay - frameTime);
+    }
+    
   }
   game->clean();
   return 0;
