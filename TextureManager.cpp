452965ef253d3ae4b29b3c6966d876a963102824
nikkaramessinis <nikaramessinis@gmail.com>
2020-05-03 13:42:38 +0100

    Changing tiles

--- TextureManager.cpp
+++ TextureManager.cpp
@@ -4,6 +4,10 @@ SDL_Texture* TextureManager::LoadTexture(const char* texture)
 {
   SDL_Surface* tempSurface = IMG_Load(texture);
   SDL_Texture* tex = SDL_CreateTextureFromSurface(Game::renderer, tempSurface);
+  if(!tex)
+  {
+    std::cout << "Couldn't load texture" << std::endl;
+  }
   SDL_FreeSurface(tempSurface);
   return tex;
 }
